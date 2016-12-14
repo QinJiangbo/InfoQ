7c4ae00d1abe042be06b7fbac3d8248eee8dd333
QinJiangbo <qinjiangbo@users.noreply.github.com>
2016-12-14 12:14:49 +0800

    add schedule tasks

--- src/main/java/com/qinjiangbo/ofo/OFOStart.java
+++ src/main/java/com/qinjiangbo/ofo/OFOStart.java
@@ -1,6 +1,9 @@
 package com.qinjiangbo.ofo;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * @date: 12/12/2016 7:26 PM
@@ -8,8 +11,31 @@ import java.io.IOException;
  */
 public class OFOStart {
 
-    public static void main(String[] args) throws IOException {
-        ScrapyOFO scrapyOFO = new ScrapyOFO();
-        scrapyOFO.request();
+    Timer timer;
+    ScrapyOFO scrapyOFO;
+
+    public OFOStart() {
+        timer = new Timer();
+        System.out.println("定时任务开始了...");
+        timer.scheduleAtFixedRate(new ReqeatTask(), 0, 30 * 60 * 1000);
+        scrapyOFO = new ScrapyOFO();
+    }
+
+    class ReqeatTask extends TimerTask {
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("@" + new Date().toLocaleString());
+                scrapyOFO.request();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new OFOStart();
     }
 }
